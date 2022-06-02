7c1549aeb1ebf0f851debf3f726f619e0840b6f4
MarahShihadeh <shihadehmarah@gmail.com>
2022-06-03 01:07:21 +0300

    login/frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import {Register} from "./component/register";
 import NavBar from "./component/navbar";
+import {Login} from "./component/login"
+ 
 function App() {
   return (
   <div className="App">Welcome APP
 <NavBar/>
   <Routes>
   <Route path={"/"} element={<Register />} />
+  <Route path={"/login"} element={<Login />} />
 
 
 
