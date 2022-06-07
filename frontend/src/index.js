5018b3275a18d89f37a800b2419fba08203a2b5c
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-06-07 23:51:45 +0300

    PayPal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
+import { Provider } from 
+'react-redux';
+import { render } from "react-dom";
 import store from "./redux/store";
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
